6eedeb37007be9d13ddadefb8fdf6c40adfaa7eb
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2020-10-13 22:05:18 +0300

    fix: adjust footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { Container, Copyright } from "./styles";
+import * as S from "./styles";
 
 const year = new Date().getFullYear();
 
 const Footer = () => {
    return (
-      <Container>
-         <Copyright>Copyright &copy; {year} OpenTable. All Rights Reserved</Copyright>
-      </Container>
+      <S.Wrapper>
+         <S.Copyright>Copyright &copy; {year} OpenTable. All Rights Reserved</S.Copyright>
+      </S.Wrapper>
    );
 };
 
