ea1c35343a83b934c744a495d3c3e74f1de1d18b
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2020-10-13 22:02:55 +0300

    feat: add styler-loader and css-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./public/index.html",
-  filename: "./index.html"
+   template: "./public/index.html",
+   filename: "./index.html",
 });
 
 module.exports = {
-  entry: ['./src/App.js'],
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'].map(require.resolve),
-            },
-          },
-        ],
-      },
-    ],
-  },
-  output: {
-    path: path.join(__dirname, '/public/webpack/'),
-    filename: 'bundle.js',
-  },
-  plugins: [htmlPlugin]
+   entry: ["./src/App.js"],
+   module: {
+      rules: [
+         {
+            test: /\.js$|jsx/,
+            exclude: /node_modules/,
+            use: [
+               {
+                  loader: "babel-loader",
+                  options: {
+                     presets: ["@babel/preset-env", "@babel/preset-react"].map(require.resolve),
+                  },
+               },
+            ],
+         },
+         {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+         },
+      ],
+   },
+   output: {
+      path: path.join(__dirname, "/public/webpack/"),
+      filename: "bundle.js",
+   },
+   plugins: [htmlPlugin],
 };
