7d7d9a541b777f63e0f95d9836d136a317a0acc1
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2020-10-13 23:07:10 +0300

    feat: 2nd attempt to host on gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       ],
    },
    output: {
-      path: path.join(__dirname, "/public/webpack/"),
+      path: path.join(__dirname, "/build"),
       filename: "bundle.js",
    },
    plugins: [htmlPlugin],
